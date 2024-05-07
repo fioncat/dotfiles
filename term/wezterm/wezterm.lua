e36496c82821783831ea506340b27a72b46ce296
fioncat <lazycat7706@gmail.com>
2024-05-08 00:18:45 +0800

    update wezterm comment

--- term/wezterm/wezterm.lua
+++ term/wezterm/wezterm.lua
@@ -13,9 +13,8 @@ config.window_padding = { left = 0, right = 0, top = 10, bottom = 0 }
 
 config.window_background_opacity = 0.97
 
--- TEMP, wait 5103 to be fixed
--- See: https://github.com/wez/wezterm/issues/5103
--- And: https://github.com/hyprwm/Hyprland/issues/4806
+-- This require: yay -S wezterm-git
+-- TODO: Wait for https://github.com/wez/wezterm/pull/5264 to be released
 config.enable_wayland = true
 
 -- https://github.com/nekowinston/wezterm-bar
