a408e44a8fd159e877102a98e261a2d90de2026d
fioncat <lazycat7706@gmail.com>
2024-12-29 16:59:58 +0800

    better command_not_found_handler

--- term/zsh/lib/tools.zsh
+++ term/zsh/lib/tools.zsh
@@ -45,24 +45,55 @@ command_not_found_handler() {
   local os_type="$(uname)"
   local output
 
-  echo "Command '$command' not found. Searching for package..."
-
+  echo "Command '$command' not found"
   if [[ "$os_type" == "Linux" ]]; then
-    output=$(yay -F --color always "usr/bin/$command" 2> /dev/null)
-    if [[ -n "$output" ]]; then
-      output=$(echo "$output" | sed "s/usr\/bin\/$command is owned by /  /g")
-      output=$(echo "Found pacman packages:\n${output}\n\nHINT: you can use 'yay -S <package>' to install one of them")
-    fi
+    command_not_found_archlinux "$command"
   elif [[ "$os_type" == "Darwin" ]]; then
-    output=$(brew search "$command" 2> /dev/null)
+    command_not_found_macos "$command"
+  fi
+  return 127
+}
+
+command_not_found_archlinux() {
+  if ! command -v yay &> /dev/null; then
+    return
   fi
 
+  local command="$1"
+  local output
+
+  echo "Seaching packages using yay..."
+  output=$(yay -F --color always "usr/bin/$command" 2> /dev/null | sed "s/usr\/bin\/$command is owned by /  /g")
   if [[ -n "$output" ]]; then
     echo
+    echo "Found pacman packages:"
     echo "$output"
+    echo
+    echo "HINT: you can use 'yay -S <package>' to install one of them"
   else
     echo "Cannot find package contains this command"
+    echo
+    echo "HINT: You can try to search this in AUR: https://aur.archlinux.org/packages?K=${command}"
+  fi
+}
+
+command_not_found_macos() {
+  if ! command -v gh &> /dev/null; then
+    return
   fi
 
-  return 127
+  local command="$1"
+  local output
+
+  echo "Searching packages from homebrew-core"
+  output=$(gh search code "$command path:Formula" --repo homebrew/homebrew-core --json path --jq '.[].path | sub("Formula/"; "") | sub(".rb$"; "") | split("/")[-1]')
+  if [[ -n "$output" ]]; then
+    echo
+    echo "Found homebrew packages (contains this keyword):"
+    echo "$output"
+    echo
+    echo "HINT: you can use 'brew install <package>' to install one of them"
+  else
+    echo "Cannot find package contains this keyword"
+  fi
 }
