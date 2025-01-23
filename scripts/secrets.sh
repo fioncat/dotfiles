826e69c72f48f0e22f19c9cb39eda09829ce277b
fioncat <lazycat7706@gmail.com>
2025-01-23 15:35:40 +0800

    support new csync

--- scripts/secrets.sh
+++ scripts/secrets.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+action="$1"
+
 set -eu
 
 if ! command -v roxide &> /dev/null; then
@@ -7,9 +9,23 @@ if ! command -v roxide &> /dev/null; then
   exit 1
 fi
 
-if [[ -f $HOME/.secrets ]]; then
-  echo "secrets file already exists, skip"
-  exit 0
-fi
-
-roxide secret dotfiles/term/zsh/secrets -f $HOME/.secrets
+case $action in
+  "save")
+    if [[ ! -f $HOME/.secrets ]]; then
+      echo "secrets file does not exist, skip"
+      exit 0
+    fi
+    roxide secret $HOME/.secrets -f dotfiles/term/zsh/secrets
+    ;;
+  "load")
+    if [[ -f $HOME/.secrets ]]; then
+      echo "secrets file already exists, skip"
+      exit 0
+    fi
+    roxide secret dotfiles/term/zsh/secrets -f $HOME/.secrets
+    ;;
+  *)
+    echo "Usage: $0 [save|load]"
+    exit 1
+    ;;
+esac
